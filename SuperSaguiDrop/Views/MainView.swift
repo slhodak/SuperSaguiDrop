bbe18632eb43b655fab578d7aefcd78eca68385e
Sam Hodak <slhodak@gmail.com>
2023-12-12 11:55:42 -0300

    Switch to NavigationStack

--- SuperSaguiDrop/Views/MainView.swift
+++ SuperSaguiDrop/Views/MainView.swift
@@ -13,21 +13,19 @@ struct MainView: View {
     @State private var isGameRunning: Bool = false
     
     var body: some View {
-        NavigationView {
-            VStack {
-                Image("ssd-logo-draft")
+        NavigationStack {
+            Image("ssd-logo-draft")
+                .resizable()
+                .frame(width: 400, height: 400)
+            
+            NavigationLink(destination: GameView(isGameRunning: $isGameRunning)) {
+                Image("ssd-play-button")
                     .resizable()
-                    .frame(width: 400, height: 400)
-                
-                NavigationLink(destination: GameView(isGameRunning: $isGameRunning)) {
-                    Image("ssd-play-button")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 200)
-                        .padding(20)
-                }
+                    .scaledToFit()
+                    .frame(width: 200)
+                    .padding(20)
             }
-            .background(Color.green)
         }
+        .background(Color.green)
     }
 }
