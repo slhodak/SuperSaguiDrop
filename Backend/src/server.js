869cc311b69a078917e8ce69822b06a8e5f250eb
Sam Hodak <slhodak@gmail.com>
2023-12-15 14:20:01 -0300

    Define score fetch api

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -9,7 +9,18 @@ const db = new Database();
 
 app.use(express.json())
 
-const validateScoreData = [
+const validateScoreDataRequest = [
+  body('user_name').isString(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  }
+];
+
+const validateScoreDataInput = [
     body('user_name').isString(),
     body('ts').isInt(),
     body('saguisSaved').isInt(),
@@ -26,33 +37,49 @@ const validateScoreData = [
 ];
 
 app.get('/', (req, res) => {
-  res.send({"response": "None"});
+  res.send({"response": "Yes?"});
 });
 
 app.get('/health', (req, res) => {
-  res.send({"response": "OK"})
-})
+  res.send({"response": "OK"});
+});
 
-app.post('/score', validateScoreData, async (req, res) => {
+app.get('/score', validateScoreDataRequest, async (req, res) => {
+  try {
+    const userName = req.body.userName;
+    const scoreData = await db.fetchScore(userName);
+    res.send({
+      "response": {
+        "un": userName,
+        "tis": scoreData.ts,
+        "ss": scoreData.saguisSaved,
+        "ot": scoreData.oncasTamed,
+        "d": scoreData.duration,
+        "tos": scoreData.totalScore,
+      }
+    });
+    
+  } catch (error) {
+    console.error(`Error while fetching score: ${error.message}`);
+    console.error(error.stack);
+    res.status(500).send({"response": "ERR"});
+  }
+});
+
+app.post('/score', validateScoreDataInput, async (req, res) => {
   try {
     const scoreData = req.body;
     // Save score to db
-    const result = await db.insertScore(scoreData)
-    res.send({"response": "OK"})
-    
+    const result = await db.insertScore(scoreData);
+    res.send({"response": "OK"});
+
   } catch (error) {
-    console.error(`Error while posting score: ${error.message}`)
-    console.error(error.stack)
-    res.send({"response": "ERR"})
+    console.error(`Error while posting score: ${error.message}`);
+    console.error(error.stack);
+    res.status(500).send({"response": "ERR"});
   }
 })
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
-
-
-// Utils
-function parseScore(scorePayload) {
-
-}
