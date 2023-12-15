0be372b038d0364842f989c6510b64973aad0893
Sam Hodak <slhodak@gmail.com>
2023-12-15 19:07:45 -0300

    Fix fetch query

--- Backend/src/database.js
+++ Backend/src/database.js
@@ -42,17 +42,19 @@ class Database {
     }
   }
 
-  async fetchScore(userName) {
+  async fetchRecentScores(userName) {
+    // TODO: Exclude scores more than a week old
     const query = `
-      SELECT (user_name, ts, saguis_saved, oncas_tamed, duration, total_score)
+      SELECT user_name, ts, saguis_saved, oncas_tamed, duration, total_score
       FROM scores
-      WHERE user_name='($1)'`;
+      WHERE user_name = $1`;
     
     try {
-      const res = await this.pool.query(query, userName);
-      console.log(res.rows[0]);
+      const res = await this.pool.query(query, [userName]);
+      return res.rows;
     } catch (err) {
       console.error(err);
+      return []
     }
   }
 }
