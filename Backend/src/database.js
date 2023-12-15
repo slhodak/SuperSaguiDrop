2bc0156e6ae52ae4d5c3cb30cdb67442e6f24fa1
Sam Hodak <slhodak@gmail.com>
2023-12-15 14:23:48 -0300

    Add fetchScore to db

--- Backend/src/database.js
+++ Backend/src/database.js
@@ -18,7 +18,7 @@ class Database {
       port: dbPort,
     });
   }
-  
+
   async insertScore(data) {
     const query = `
       INSERT INTO scores (user_name, ts, saguis_saved, oncas_tamed, duration, total_score)
@@ -36,6 +36,20 @@ class Database {
     
     try {
       const res = await this.pool.query(query, values);
+      console.log(res.rows[0]);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async fetchScore(userName) {
+    const query = `
+      SELECT (user_name, ts, saguis_saved, oncas_tamed, duration, total_score)
+      FROM scores
+      WHERE user_name='($1)'`;
+    
+    try {
+      const res = await this.pool.query(query, userName);
       console.log(res.rows[0]);
     } catch (err) {
       console.error(err);
